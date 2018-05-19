c1f1406a204aa3d3d0da93e51e5b3af994793c78
BSvetlana <svetlanabisacki@gmail.com>
2018-05-19 15:01:56 +0200

    change commit-5

--- src/components/TriviaApp.vue
+++ src/components/TriviaApp.vue
@@ -1,24 +1,33 @@
 <template>
     <div>
-        <b-list-group>
+      <div class="container mt-4">
+        <b-container class="bv-example-row">
+          <b-row class="justify-content-md-center">
+            <b-col cols="10"> 
+              <b-list-group>
 
-            <b-list-group-item v-for="trivia in trivias" :key="trivia.id">
-                {{trivia.question}}
-                    <b-button type="submit"
-                         
-                        @click="gotAnswer(trivia.id)"
-                        style="float: right"
-                        v-b-modal.modal1>
-                        Answer
-                    </b-button>
+                  <b-list-group-item v-for="trivia in trivias" :key="trivia.id">
+                      {{trivia.question}}
+                          <b-button type="submit"
+                              
+                              @click="gotAnswer(trivia.id)"
+                              style="float: right"
+                              v-b-modal.modal1>
+                              Answer
+                          </b-button>
 
-            </b-list-group-item>
+                  </b-list-group-item>
 
-        </b-list-group>
+              </b-list-group>
 
-        <b-modal id="modal1" title="Answer">
-            <p class="my-4">{{ triviaAnswer }}</p>
-        </b-modal>
+              <b-modal id="modal1" title="Answer">
+                  <p class="my-4">{{ triviaAnswer }}</p>
+              </b-modal>
+
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
 
     </div>
 </template>
@@ -43,15 +52,15 @@ export default {
   },
   methods: {
     ...mapActions(["fetchTrivias"]),
-    toggleTrivia(triviaId) {
-      let triviaIdIndex = this.selectedTriviasId.indexOf(triviaId);
-      let isSelectedTriviaId = this.selectedTriviasId.indexOf(triviaId) > -1;
+    // toggleTrivia(triviaId) {
+    //   let triviaIdIndex = this.selectedTriviasId.indexOf(triviaId);
+    //   let isSelectedTriviaId = this.selectedTriviasId.indexOf(triviaId) > -1;
 
-      if (isSelectedTriviaId) {
-        return this.selectedTriviasId.splice(triviaIdIndex, 1);
-      }
-      this.selectedTriviasId.push(triviaId);
-    },
+    //   if (isSelectedTriviaId) {
+    //     return this.selectedTriviasId.splice(triviaIdIndex, 1);
+    //   }
+    //   this.selectedTriviasId.push(triviaId);
+    // },
     gotAnswer(id) {
       let trivias = this.trivias.filter(trivia => {
         return trivia.id === id;
